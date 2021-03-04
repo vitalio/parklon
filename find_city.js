a52042201ba3781e072f495d459cf7d947ca9f53
vitalio <2vitalio@gmail.com>
2021-03-04 09:30:26 +0300

    move all public to root

---
+++ find_city.js
@@ -0,0 +1,13 @@
+let cities;
+
+async function init(){
+    console.log('XXX init');
+    const res = await fetch('./cities.json');
+    cities = await res.json();
+    console.log('XXX cities', cities);
+}
+init();
+
+function init_ui(){
+    console.log('XXX ui inited');
+}
