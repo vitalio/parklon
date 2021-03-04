57277b2f80e0a886a75ef38e82450e24358a38c3
vitalio <2vitalio@gmail.com>
2021-03-04 12:28:21 +0300

    added copy button; added load_data

--- find_city/find_city.js
+++ find_city/find_city.js
@@ -1,14 +1,42 @@
 let cities;
 
 async function init(){
-    console.log('XXX init');
-    // const res = await fetch('../cities.json');
-    // cities = await res.json();
-    // console.log('XXX cities', cities);
-    $('#city').autoComplete({
-        resolverSettings: {
-            url: '../cities.json'
-        }
+    const res = await fetch('../cities.json');
+    cities = await res.json();
+    const cities_datasrc = [];
+    for (const id in cities)
+        cities_datasrc.push({label: cities[id], value: id});
+    const ac = new Autocomplete(document.getElementById('city'), {
+        data: cities_datasrc,
+        maximumItems: 10,
+        treshold: 1,
+        onSelectItem: ({label, value}) =>{
+            console.log('user selected:', label, value);
+            const data = await load_data(value);
+            // set_result(`Label: ${label}\nValue: ${value}\nDate: ${Date.now()}`);
+            copy_result();
+        },
     });
 }
-init();
+
+async function load_data(id){
+    const res = await restdb_query('city', {id});
+    const data = res && res[0];
+    console.log(JSON.stringify(data));
+}
+
+function set_result(text){
+    $('#result').text(text);
+}
+
+function copy_result(){
+    const copy_text = document.getElementById('result');
+    const input = document.createElement('textarea');
+    input.value = copy_text.textContent;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('Copy');
+    input.remove();
+}
+
+$(document).ready(init);
